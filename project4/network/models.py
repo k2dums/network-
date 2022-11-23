5cbf09d86571616f1517078a9b94466c34f65f15
k2dums <norbu.paddu@gmail.com>
2022-11-23 16:27:14 +0530

    added models for Like,Post,Comment,Follower

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="posts")
+    img_url=models.CharField(max_length=200,null=True)
+    description=models.CharField(max_length=200,null=True)
+    hasPhoto=models.BooleanField(default=False)
+
+class Like(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="likes")
+
+class Comment(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    comment=models.CharField(max_length=200,null=True)
+
+class Follower(models.Model):
+    follow=models.ForeignKey(User,on_delete=models.CASCADE,related_name="followers")
+    followers=models.ManyToManyField(User,blank=True,related_name='following')
