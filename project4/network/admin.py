2b8b1fc6d08c65cc7a169a91a0c772c600995daa
k2dums <norbu.paddu@gmail.com>
2022-11-23 16:28:26 +0530

    register the newly created Like,post,comment,Follower model, created a filter_horizontal for Follower

--- project4/network/admin.py
+++ project4/network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User,Like,Comment,Follower,Post
+
+class FollowerAdmin(admin.ModelAdmin):
+        filter_horizontal=('followers',)
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Follower,FollowerAdmin)
+admin.site.register(Post)
