767805a9a904723d94d7dbfca56ee67cc5925ef5
k2dums <norbu.paddu@gmail.com>
2022-11-26 22:52:07 +0530

    added paths for profile and following

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("profile",views.profile,name="profile"),
+    path("user/<str:username>",views.profile,name="profile"),
+    path("following/<str:username>",views.following,name="following")
 ]
