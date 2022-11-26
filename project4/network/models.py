05e3844b3932a9728273f5965ed1430e7bfe79a0
k2dums <norbu.paddu@gmail.com>
2022-11-26 13:02:20 +0530

    added str dunder to all the class models

--- project4/network/models.py
+++ project4/network/models.py
@@ -11,18 +11,27 @@ class Post(models.Model):
     time=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return(f"Post {self.id} by {self.user} at [{self.time}]")
+        return(f"Post {self.id} by {self.user} ")
 
 
 class Like(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="likes")
 
+    def __str__(self):
+        return(f'[{self.post}]liked by {self.user}')
+
 class Comment(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     post=models.ForeignKey(Post,on_delete=models.CASCADE)
     comment=models.CharField(max_length=200,null=True)
 
+    def __str__(self):
+        return (f"[{self.post}] comment by {self.user}")
+
 class Follower(models.Model):
     follow=models.ForeignKey(User,on_delete=models.CASCADE,related_name="followers")
     followers=models.ManyToManyField(User,blank=True,related_name='following')
+
+    def __str__(self):
+        return (f"{self.followers}is following {self.follow}")
