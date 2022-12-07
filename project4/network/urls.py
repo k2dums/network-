03ce4d6097b3f307a8baef342c0a83790b6b38da
k2dums <norbu.paddu@gmail.com>
2022-12-07 17:00:17 +0530

    changed the paramater name to post_id for the post path

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("user/<str:username>",views.profile,name="profile"),
     path("following/<str:username>",views.following,name="following"),
-    path("post/<int:post>",views.post,name="post"),
+    path("post/<int:post_id>",views.post,name="post"),
 ]
