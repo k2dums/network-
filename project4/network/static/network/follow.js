dabf99192ed0f963bc635de184f03a89700aed82
k2dums <norbu.paddu@gmail.com>
2022-12-07 17:07:02 +0530

    edit_action() that handles onclick event, removes text,adds text area, adds cancel and save button.The like_action() now handles the reuslt from the put method and toggeles the class to change icon and updates the likes.The cancel_action() replaces and adds the necessary elements.

--- project4/network/static/network/follow.js
+++ project4/network/static/network/follow.js
@@ -1,15 +1,50 @@
-// document.addEventListener('DOMContentLoaded',function(){
-//     if (document.querySelector("#follow_btn"))
-//     {document.querySelector("#follow_btn").addEventListener('click',()=>follow_action("follow"));
-//     }
+document.addEventListener('DOMContentLoaded',function(){
+});
 
-//     if (document.querySelector("#unfollow_btn"))
-//     {document.querySelector("#unfollow_btn").addEventListener('click',()=>follow_action("unfollow"));
-//     }
-   
+function edit_action(e,post)
+{   console.log('edit has been clicked')
+    const button =e.target;
+    const parent=button.parentElement;
+    fetch(`/post/${post}`)
+    .then(res=>res.json())
+    .then(result=>{
+        console.log(result);
+        const text=result.post;
+        // the text_area 
+        const  edit_text_area=document.createElement('textarea');
+        edit_text_area.classList.add('edit_text_area')
+        edit_text_area.value=text;
+        //Save button
+        const save=document.createElement('button');
+        save.innerHTML='Save Edit';
+        save.classList.add('btn','btn-primary','text-end','save_button','float-right');
+        //cancel button
+        const cancel=document.createElement('Cancel');
+        cancel.innerHTML='Cancel'
+        cancel.classList.add('btn','btn-outline-danger','float-left')
+        //wrapper for the button needed for clearfix
+        const edit_button_wrapper=document.createElement('div');
+        edit_button_wrapper.classList.add('save_wrapper','clearfix','mt-2');
+        edit_button_wrapper.append(cancel);
+        edit_button_wrapper.append(save);
+        post_node=parent.closest('.post');
+        for(const child of post_node.children){
+            if (child.className=='post_content')
+            {   //child is post_content node
 
-// });
-
+                child.innerHTML="";
+                child.append(edit_text_area);
+                console.log(child.style.color);
+                child.style.backgroundColor= 'transparent';
+                child.append(edit_button_wrapper);
+                cancel.addEventListener('click',()=>cancel_action(text,child,button,post_node))
+                button.remove();
+                break;
+               
+            }
+        }
+    })
+}
 function follow_action(action){
     let follow=window.location.href;
     const by=document.querySelector(".user_logged_in").innerHTML;
@@ -36,16 +71,33 @@ function follow_action(action){
 }
 
 
-function like_action(post){
-fetch(`/post/${post}`,{
-    method:'PUT',
-}).then(response=>console.log(response));
+function like_action(e,post){
+    const button=e.target;
+    fetch(`/post/${post}`,{
+        method:'PUT'
+    })
+        .then(res=>res.json())
+        .then(result=>{
+            button.classList.toggle('fa-solid')
+            childrens= button.parentElement.parentElement.children
+            childrens[1].innerHTML=result.likes
+            }
+        );
 }
 
-function change_like_icon(){
-    document.querySelectorAll(".like_button").forEach(button=>{
-        button.ondblclick=function(){
-         this.classList.toggle('fa-solid');
-        }
-    });
+function cancel_action(text,post_content_node,edit_button,post_node)
+{   
+    post_content_node.innerHTML=" ";
+    text=text.replaceAll('\n','<br>')
+    post_content_node.innerHTML=text;
+    post_content_node.style.backgroundColor='rgba(219, 214, 214, 0.4)';
+    const node=post_node.querySelector(".edit_button_wrapper");
+    console.log(edit_button);
+    console.log(node);
+    node.append(edit_button);
+}
+
+function save_action()
+{
+
 }
