a27b649049cbe37e68a683ebcedad416c8f60441
k2dums <norbu.paddu@gmail.com>
2022-12-08 23:12:44 +0530

    Added admin interface for post and like model

--- project4/network/admin.py
+++ project4/network/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 
 from .models import User,Like,Comment,Follower,Post
 
+class PostAdmin(admin.ModelAdmin):
+        filter_horizontal=('like',)
 class FollowerAdmin(admin.ModelAdmin):
         list_display=("follow","by")
 # Register your models here.
+class LikeAdmin(admin.ModelAdmin):
+        list_display= ('post','user','flag')
 admin.site.register(User)
-admin.site.register(Like)
+admin.site.register(Like,LikeAdmin)
 admin.site.register(Comment)
 admin.site.register(Follower,FollowerAdmin)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
